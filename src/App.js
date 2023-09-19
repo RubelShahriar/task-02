b3dade1ae48cb046885cca27b6e47548a1004cd7
RubelShahriar <mr.rubelshahriar@gmail.com>
2023-09-19 15:02:11 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import initalData from "./InitialData";
+import EmojiCursor from "./Cursor";
 
 function App() {
+  const [isOn, setIsOn] = useState(false);
+  const [userData, setUserData] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    textarea: initalData,
+    checkbox: false,
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    textarea: "",
+    checkbox: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    // Perform validation based on input type
+    const validationErrors = { ...errors };
+
+    switch (name) {
+      case "name":
+        validationErrors.name =
+          value.length < 3 || value.length > 20
+            ? "⚠️ Name must be between 3 and 20 characters"
+            : value.trim() !== value
+            ? "⚠️ Name cannot have leading or trailing spaces."
+            : !/^[a-zA-Z-' ]+$/.test(value)
+            ? "⚠️ Name can only contain letters, spaces, hyphens, and apostrophes."
+            : "";
+        break;
+      case "textarea":
+        validationErrors.textarea =
+          value.length < 5
+            ? "⚠️ Value must be more than 5 characters"
+            : !value.trim()
+            ? "⚠️ Textarea cannot be empty."
+            : !/.+\n.+/s.test(value)
+            ? "⚠️ Enter atleast 2 sector"
+            : "";
+        break;
+      case "checkbox":
+        validationErrors.checkbox = !checked
+          ? "⚠️ You must accept the terms and conditions"
+          : "";
+        break;
+      default:
+        break;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+    setErrors(validationErrors);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if there are any validation errors
+    const { name, textarea, checkbox } = formData;
+    if (
+      !name ||
+      !textarea ||
+      !checkbox ||
+      Object.values(errors).some((error) => error !== "")
+    ) {
+      alert("Please fill in all required fields correctly.");
+      return;
+    }
+
+    // Send formData to a backend server (e.g., Express.js) and store it in MongoDB
+    try {
+      await fetch("http://localhost:4000/insert-and-return", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUserData(data); // Update the state with the newly added item
+        })
+        .catch((error) => console.error("Error:", error));
+
+      // alert a response
+      alert("Data stored successfully");
+
+      // update the state values from database
+      setFormData({
+        name: userData.name,
+        textarea: userData.textarea,
+        checkbox: userData.checkbox,
+      });
+    } catch (error) {
+      console.error("Error storing data:", error);
+    }
+    console.log(errors);
+  };
+
+  const handleToggleClick = () => {
+    setIsOn(!isOn);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>
+          Please enter your name and edit the sectors you are currently involved
+          in.
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <div className="error">{errors.name}</div>
+          <br />
+          <label>Sectors:</label>
+          <textarea
+            name="textarea"
+            rows={20}
+            value={formData.textarea}
+            onChange={handleChange}
+            required
+          />
+          <div className="error">{errors.textarea}</div>
+          <br />
+          {userData.checkbox ? (
+            <input
+              type="checkbox"
+              name="checkbox"
+              checked={formData.checkbox}
+              onChange={handleChange}
+              required
+            />
+          ) : (
+            <input
+              type="checkbox"
+              name="checkbox"
+              checked={userData.checkbox}
+              onChange={handleChange}
+              required
+            />
+          )}{" "}
+          Agree to terms
+          <div className="error">{errors.checkbox}</div>
+          <br />
+          <button type="submit">Save</button>
+        </form>
+      </div>
+      {isOn ? (
+        <button className="curBtn" onClick={handleToggleClick}>
+          Turn Off Emoji Cursor
+        </button>
+      ) : (
+        <button className="curBtn" onClick={handleToggleClick}>
+          Turn On Emoji Cursor
+        </button>
+      )}
+      {isOn && <EmojiCursor></EmojiCursor>}
+    </>
   );
 }
 
---
+++ src/Cursor.js
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+
+class EmojiCursor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      possibleEmoji: ["😀", "😂", "😆", "😊"],
+      cursor: { x: 0, y: 0 },
+      particles: [],
+    };
+  }
+
+  componentDidMount() {
+    this.bindEvents();
+    this.startAnimation();
+  }
+
+  componentWillUnmount() {
+    this.removeEventListeners();
+  }
+
+  bindEvents() {
+    document.addEventListener("mousemove", this.handleMouseMove);
+    document.addEventListener("touchmove", this.handleTouchMove);
+    document.addEventListener("touchstart", this.handleTouchMove);
+  }
+
+  removeEventListeners() {
+    document.removeEventListener("mousemove", this.handleMouseMove);
+    document.removeEventListener("touchmove", this.handleTouchMove);
+    document.removeEventListener("touchstart", this.handleTouchMove);
+  }
+
+  handleTouchMove = (e) => {
+    if (e.touches.length > 0) {
+      for (let i = 0; i < e.touches.length; i++) {
+        this.addParticle(
+          e.touches[i].clientX,
+          e.touches[i].clientY,
+          this.getRandomEmoji()
+        );
+      }
+    }
+  };
+
+  handleMouseMove = (e) => {
+    this.setState({
+      cursor: { x: e.clientX, y: e.clientY },
+    });
+
+    this.addParticle(e.clientX, e.clientY, this.getRandomEmoji());
+  };
+
+  getRandomEmoji() {
+    const { possibleEmoji } = this.state;
+    return possibleEmoji[Math.floor(Math.random() * possibleEmoji.length)];
+  }
+
+  addParticle(x, y, character) {
+    const particle = {
+      lifeSpan: 120,
+      velocity: {
+        x: (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
+        y: 1,
+      },
+      position: { x: x - 10, y: y - 20 },
+      character: character,
+    };
+
+    this.setState((prevState) => ({
+      particles: [...prevState.particles, particle],
+    }));
+  }
+
+  updateParticles() {
+    this.setState((prevState) => ({
+      particles: prevState.particles
+        .map((particle) => ({
+          ...particle,
+          position: {
+            x: particle.position.x + particle.velocity.x,
+            y: particle.position.y + particle.velocity.y,
+          },
+          lifeSpan: particle.lifeSpan - 1,
+        }))
+        .filter((particle) => particle.lifeSpan >= 0),
+    }));
+  }
+
+  startAnimation = () => {
+    requestAnimationFrame(this.startAnimation);
+    this.updateParticles();
+  };
+
+  render() {
+    const { particles } = this.state;
+
+    return (
+      <div className="ec">
+        {particles.map((particle, index) => (
+          <span
+            key={index}
+            style={{
+              position: "absolute",
+              display: "block",
+              pointerEvents: "none",
+              zIndex: "10000000",
+              fontSize: "16px",
+              transform: `translate3d(${particle.position.x}px, ${
+                particle.position.y
+              }px, 0) scale(${particle.lifeSpan / 120})`,
+            }}
+          >
+            {particle.character}
+          </span>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default EmojiCursor;
